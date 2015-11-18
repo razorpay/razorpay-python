0a5376104e1e7c202a3f5890b4b4732f7a95e40f
Akash Kothawale <akash@decached.com>
2015-11-18 17:47:02 +0530

    `params` are optional args to Refund functions

--- razorpay/resources/refund.py
+++ razorpay/resources/refund.py
@@ -3,11 +3,11 @@ class Refund:
     def __init__(self, client=None):
         self.client = client
 
-    def all(self, payment_id, params={}, **options):
-        return self.client.get('/payments/' + payment_id + '/refunds', params, **options)
+    def all(self, payment_id, **options):
+        return self.client.get('/payments/' + payment_id + '/refunds', **options)
 
-    def fetch(self, payment_id, refund_id, params={}, **options):
-        return self.client.get('/payments/' + payment_id + '/refunds/' + refund_id, params, **options)
+    def fetch(self, payment_id, refund_id, **options):
+        return self.client.get('/payments/' + payment_id + '/refunds/' + refund_id, **options)
 
     def create(self, payment_id, data, **options):
         return self.client.post('/payments/' + payment_id + '/refund', data, **options)
