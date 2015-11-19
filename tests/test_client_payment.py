080ef25eb03f73776e3e8b190abd32ef58924c0b
Akash Kothawale <akash@decached.com>
2015-11-19 15:22:33 +0530

    Tests: capture payment, partial refunds

--- tests/test_client_payment.py
+++ tests/test_client_payment.py
@@ -18,5 +18,11 @@ class TestClientPayment(ClientTestCase):
     @responses.activate
     def test_payment_fetch(self):
         result = mock_file('fake_payment')
-        responses.add(GET, 'https://api.razorpay.com/v1/payments/fake_payment_id', status=200, body=json.dumps(result), match_querystring=True)
+        responses.add(GET, 'https://api.razorpay.com/v1/payments/' + self.payment_id, status=200, body=json.dumps(result), match_querystring=True)
         self.assertEqual(self.client.payment.fetch('fake_payment_id'), result)
+
+    @responses.activate
+    def test_payment_capture(self):
+        result = mock_file('fake_captured_payment')
+        responses.add(POST, 'https://api.razorpay.com/v1/payments/' + self.payment_id + '/capture', status=200, body=json.dumps(result), match_querystring=True)
+        self.assertEqual(self.client.payment.capture(self.payment_id, amount=5100), result)
--- tests/test_client_refund.py
+++ tests/test_client_refund.py
@@ -14,8 +14,15 @@ class TestClientRefund(ClientTestCase):
         result = mock_file('fake_refund')
         responses.add(GET, 'https://api.razorpay.com/v1/payments/' + self.payment_id + '/refunds/' + self.refund_id, status=200, body=json.dumps(result), match_querystring=True)
         self.assertEqual(self.client.refund.fetch(self.payment_id, self.refund_id), result)
+
     @responses.activate
     def test_refund_create(self):
+        result = mock_file('fake_refund')
+        responses.add(POST, 'https://api.razorpay.com/v1/payments/' + self.payment_id + '/refund', status=200, body=json.dumps(result), match_querystring=True)
+        self.assertEqual(self.client.refund.create(self.payment_id, {}), result)
+
+    @responses.activate
+    def test_refund_create_partial(self):
         result = mock_file('fake_refund')
         responses.add(POST, 'https://api.razorpay.com/v1/payments/' + self.payment_id + '/refund', status=200, body=json.dumps(result), match_querystring=True)
         self.assertEqual(self.client.refund.create(self.payment_id, {'amount': 2000}), result)
