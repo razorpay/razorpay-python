9c99dc974481b273aa85c81dbc991a95648f8679
Abhay Rana <nemo@razorpay.com>
2015-12-19 16:28:27 +0530

    Adds razorpay subdirectory to MANIFEST

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="razorpay",
-    version="0.1.0",
+    version="0.1.1",
     description="Razorpay Python Client",
     url="https://github.com/decached/razorpay",
     author="Akash Kothawale",
