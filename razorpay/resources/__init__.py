41e021ce97abdf1336754d19bdc66b42b2c26398
gaurav <gaurav.d@razorpay.com>
2016-12-03 18:06:27 +0530

    [invoice] added support for invoice in python client

--- razorpay/resources/__init__.py
+++ razorpay/resources/__init__.py
@@ -1,10 +1,12 @@
 from .payment import Payment
 from .refund import Refund
 from .order import Order
+from .invoice import Invoice
 
 
 __all__ = [
         'Payment',
         'Refund',
         'Order',
+        'Invoice',
 ]
---
+++ razorpay/resources/invoice.py
@@ -0,0 +1,43 @@
+from .base import Resource
+
+
+class Invoice(Resource):
+    def __init__(self, client=None):
+        self.client = client
+        self.base_url = "/invoices"
+
+    def fetch_all(self, **kwargs):
+        """"
+        Fetch all Invoice entities
+
+        Returns:
+            Dictionary of Invoice data
+        """
+        url = self.base_url
+        return self.get_url(url, **kwargs)
+
+    def fetch(self, invoice_id, **kwargs):
+        """"
+        Fetch Invoice for given Id
+
+        Args:
+            invoice_id : Id for which invoice object has to be retrieved
+
+        Returns:
+            Invoice dict for given invoice Id
+        """
+        url = "{}/{}".format(self.base_url, invoice_id)
+        return self.get_url(url, **kwargs)
+
+    def create(self, data={}, **kwargs):
+        """"
+        Create Invoice from given dict
+
+        Args:
+            data : Dictionary having keys using which invoice have to be created
+
+        Returns:
+            Invoice Dict which was created
+        """
+        url = self.base_url
+        return self.post_url(url, data, **kwargs)
