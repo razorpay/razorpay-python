f83c369779d6489ed707659719ab7ec9ca32003d
gaurav <gaurav.d@razorpay.com>
2016-12-12 17:14:51 +0530

    [invoice] added test cases

--- tests/test_client_invoice.py
+++ tests/test_client_invoice.py
@@ -4,10 +4,10 @@ import json
 from .helpers import mock_file, ClientTestCase
 
 
-class TestClientOrder(ClientTestCase):
+class TestClientInvoice(ClientTestCase):
 
     def setUp(self):
-        super(TestClientOrder, self).setUp()
+        super(TestClientInvoice, self).setUp()
         self.base_url = '{}/invoices'.format(self.base_url)
 
     @responses.activate
@@ -16,7 +16,7 @@ class TestClientOrder(ClientTestCase):
         url = self.base_url
         responses.add(responses.GET, url, status=200,
                       body=json.dumps(result), match_querystring=True)
-        self.assertEqual(self.client.order.fetch_all(), result)
+        self.assertEqual(self.client.invoice.fetch_all(), result)
 
     @responses.activate
     def test_invoice_fetch_all_with_options(self):
@@ -25,7 +25,7 @@ class TestClientOrder(ClientTestCase):
         url = '{}?count={}'.format(self.base_url, count)
         responses.add(responses.GET, url, status=200, body=json.dumps(result),
                       match_querystring=True)
-        self.assertEqual(self.client.order.fetch_all(count=count), result)
+        self.assertEqual(self.client.invoice.fetch_all(count=count), result)
 
     @responses.activate
     def test_invoice_fetch(self):
@@ -33,5 +33,5 @@ class TestClientOrder(ClientTestCase):
         url = '{}/{}'.format(self.base_url, 'fake_invoice_id')
         responses.add(responses.GET, url, status=200, body=json.dumps(result),
                       match_querystring=True)
-        self.assertEqual(self.client.order.fetch('fake_invoice_id'), result)
+        self.assertEqual(self.client.invoice.fetch('fake_invoice_id'), result)
 
