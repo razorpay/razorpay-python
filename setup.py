0ed97b6da8806c291012f8aa3e2360d39937d2f6
gaurav <gaurav.d@razorpay.com>
2016-12-12 18:23:43 +0530

    minor version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="razorpay",
-    version="0.2.0",
+    version="0.2.1",
     description="Razorpay Python Client",
     url="https://github.com/razorpay/razorpay-python",
     author="Team Razorpay",
