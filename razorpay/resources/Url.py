c2125ef75a966b045d7777d0ee588a70a920334e
Ria Garg <ria.garg@razorpay.com>
2017-02-13 13:36:19 +0530

    Added Card Entity

--- razorpay/resources/Url.py
+++ razorpay/resources/Url.py
@@ -2,3 +2,4 @@ class URL(object):
     ORDER_URL = "/orders"
     INVOICE_URL = "/invoices"
     PAYMENTS_URL = "/payments"
+    CARD_URL = "/cards";
---
+++ razorpay/resources/card.py
@@ -0,0 +1,31 @@
+from .base import Resource
+from .Url import URL
+
+
+class Card(Resource):
+    def __init__(self, client=None):
+        self.client = client
+        self.base_url = URL.CARD_URL
+
+    def fetch_all(self, **kwargs):
+        """"
+        Fetch all Card entities
+
+        Returns:
+            Dictionary of Card data
+        """
+        url = self.base_url
+        return self.get_url(url, **kwargs)
+
+    def fetch(self, card_id, **kwargs):
+        """"
+        Fetch Card for given Id
+
+        Args:
+            card_id : Id for which card object has to be retrieved
+
+        Returns:
+            Card dict for given card Id
+        """
+        url = "{}/{}".format(self.base_url, card_id)
+        return self.get_url(url, **kwargs)
