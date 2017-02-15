f836e157b5a74541127a429036e6b53ccd63ec55
gaurav <gaurav.d@razorpay.com>
2017-02-15 15:48:29 +0530

    [python-sdk] take params instead of individual ids

--- razorpay/utility/utility.py
+++ razorpay/utility/utility.py
@@ -6,7 +6,10 @@ class Utility(object):
     def __init__(self, client=None):
         self.client = client
 
-    def validate_signature(self, order_id, payment_id, razorpay_signature):
+    def validate_signature(self, parameters):
+        order_id = parameters['order_id']
+        payment_id = parameters['payment_id']
+        razorpay_signature = parameters['razorpay_signature']
         msg = "{}|{}".format(order_id, payment_id)
         dig = hmac.new(key=self.client.auth[1],
                        msg=msg,
