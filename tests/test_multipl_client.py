0bbb491dd6b1b327ef7feb1117afa1ddb3ebcfff
gaurav <gaurav.d@razorpay.com>
2017-03-09 14:24:51 +0530

    [python-sdk] added test case for multiple client

---
+++ tests/test_multipl_client.py
@@ -0,0 +1,38 @@
+import responses
+import json
+
+from .helpers import mock_file, ClientTestCase
+
+
+class TestClientPayment(ClientTestCase):
+
+    def setUp(self):
+        super(TestClientPayment, self).setUp()
+        self.base_url = '{}/payments'.format(self.base_url)
+        self.secondary_base_url = '{}/payments'.format(self.secondary_url)
+
+    @responses.activate
+    def test_payment_primary_url(self):
+        result = mock_file('payment_collection')
+        url = self.base_url
+        responses.add(responses.GET, url, status=200,
+                      body=json.dumps(result), match_querystring=True)
+        self.assertEqual(self.client.payment.all(), result)
+
+    @responses.activate
+    def test_payment_secondary_url(self):
+        result = mock_file('payment_collection')
+        url = self.secondary_base_url
+        responses.add(responses.GET, url, status=200,
+                      body=json.dumps(result), match_querystring=True)
+        self.assertEqual(self.secondary_client.payment.all(), result)
+
+    @responses.activate
+    def test_payment_with_headers(self):
+        result = mock_file('payment_collection')
+        url = self.base_url
+        responses.add(responses.GET, url, status=200,
+                      body=json.dumps(result), match_querystring=True)
+        self.assertEqual(self.client.payment.all(
+            headers={'Content-type': 'text'}),
+            result)
