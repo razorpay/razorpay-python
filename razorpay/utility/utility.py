cd8b9b5e8a2111be7f4b3845e46237b0545cf127
gaurav <gaurav.d@razorpay.com>
2017-03-09 12:58:48 +0530

    [python-sdk] added webhook signature validation

--- razorpay/utility/utility.py
+++ razorpay/utility/utility.py
@@ -14,18 +14,26 @@ class Utility(object):
         order_id = str(parameters['razorpay_order_id'])
         payment_id = str(parameters['razorpay_payment_id'])
         razorpay_signature = str(parameters['razorpay_signature'])
+
         msg = "{}|{}".format(order_id, payment_id)
+
+        self.verify_signature(razorpay_signature, msg)
+
+    def verify_webhook_signature(self, signature, body):
+        self.verify_signature(signature, body)
+
+    def verify_signature(self, signature, body):
         key = self.client.auth[1]
 
         if sys.version_info[0] == 3:
             key = bytes(key, 'utf-8')
-            msg = bytes(msg, 'utf-8')
+            body = bytes(body, 'utf-8')
 
         dig = hmac.new(key=key,
-                       msg=msg,
+                       msg=body,
                        digestmod=hashlib.sha256)
 
         generated_signature = dig.hexdigest()
 
-        if not hmac.compare_digest(generated_signature, razorpay_signature):
+        if not hmac.compare_digest(generated_signature, signature):
             raise SignatureVerificationError('Payment Signature Verification Failed')
