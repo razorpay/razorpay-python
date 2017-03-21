862583696e0af9b7ce349615fe9cf2dacf2c9786
mayankamencherla <mayank.amencherla@razorpay.com>
2017-03-21 13:23:06 +0530

    [app details] Added test case to cover app_details method

---
+++ tests/test_user_agent.py
@@ -0,0 +1,21 @@
+import responses
+import json
+import yaml
+
+from .helpers import mock_file, ClientTestCase
+
+class TestClientUserAgent(ClientTestCase):
+
+    def setUp(self):
+        super(TestClientUserAgent, self).setUp()
+        app_details = yaml.safe_load(mock_file('fake_app_details'))
+        self.client.set_app_details(app_details)
+        self.base_url = "{}/payments".format(self.base_url)
+
+    @responses.activate
+    def test_payment_all(self):
+        result = mock_file('payment_collection')
+        url = self.base_url
+        responses.add(responses.GET, url, status=200,
+                      body=json.dumps(result), match_querystring=True)
+        self.assertEqual(self.client.payment.all(), result)
