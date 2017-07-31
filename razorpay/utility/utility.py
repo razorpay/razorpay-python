39c65f6d0eeb33568dc92b0b2c1b8dc8d4ea0f3e
gaurav <gaurav.d@razorpay.com>
2017-07-31 18:34:39 +0530

    for python version < 2.7.7 implement const time str comparison (#29)

--- razorpay/utility/utility.py
+++ razorpay/utility/utility.py
@@ -35,5 +35,29 @@ class Utility(object):
 
         generated_signature = dig.hexdigest()
 
-        if not hmac.compare_digest(generated_signature, signature):
-            raise SignatureVerificationError('Payment Signature Verification Failed')
+        if sys.version_info[0:3] < (2, 7, 7):
+            result = self.compare_string(generated_signature, signature)
+        else:
+            result = hmac.compare_digest(generated_signature, signature)
+
+        if not result:
+            raise SignatureVerificationError(
+                'Razorpay Signature Verification Failed')
+
+    # Taken from Django Source Code
+    # Used in python version < 2.7.7
+    # As hmac.compare_digest is not present in prev versions
+    def compare_string(self, expected_str, actual_str):
+        """
+        Returns True if the two strings are equal, False otherwise
+        The time taken is independent of the number of characters that match
+        For the sake of simplicity, this function executes in constant time only
+        when the two strings have the same length. It short-circuits when they
+        have different lengths
+        """
+        if len(expected_str) != len(actual_str):
+            return False
+        result = 0
+        for x, y in zip(expected_str, actual_str):
+            result |= ord(x) ^ ord(y)
+        return result == 0
