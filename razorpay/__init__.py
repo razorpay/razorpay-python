b5fab901c36ce591706e73cde87390f450580579
gaurav <gaurav.d@razorpay.com>
2017-09-30 02:30:09 +0530

    [python-sdk] added virtual account

--- razorpay/__init__.py
+++ razorpay/__init__.py
@@ -6,6 +6,7 @@ from .resources import Invoice
 from .resources import Customer
 from .resources import Card
 from .resources import Token
+from .resources import VirtualAccount
 from .utility import Utility
 from .constants import ERROR_CODE
 from .constants import HTTP_STATUS_CODE
@@ -20,6 +21,7 @@ __all__ = [
         'Customer',
         'Card',
         'Token',
+        'VirtualAccount',
         'HTTP_STATUS_CODE',
         'ERROR_CODE',
 ]
