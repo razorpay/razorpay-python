0dd46b98b1ffa1d25a02748d26dce91ba16a14d0
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2020-07-22 02:14:59 +0530

    Included README as 'long_description' in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open('README.md') as readme:
+    readme_content = readme.read()
+
 setup(
     name="razorpay",
     version="1.2.0",
     description="Razorpay Python Client",
+    long_description=readme_content,
+    long_description_content_type='text/markdown',
     url="https://github.com/razorpay/razorpay-python",
     author="Team Razorpay",
     license="MIT",
