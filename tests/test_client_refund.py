dfc8c55af2ac56cdba6e62ada407051f5453f1c7
apatra9 <a.patra56789@gmail.com>
2021-05-27 16:52:26 +0530

    test case edit

--- tests/test_client_refund.py
+++ tests/test_client_refund.py
@@ -8,8 +8,8 @@ class TestClientRefund(ClientTestCase):
 
     def setUp(self):
         super(TestClientRefund, self).setUp()
+        self.payment_url = '{}/payments'.format(self.base_url)
         self.base_url = '{}/refunds'.format(self.base_url)
-        self.payment_url = '{}/payments'.format(self.payment_url)
 
     @responses.activate
     def test_refund_all(self):
@@ -30,8 +30,8 @@ class TestClientRefund(ClientTestCase):
     @responses.activate
     def test_refund_fetch_for_payment(self):
         result = mock_file('fake_refund')
-        url = '{}/{}{}'.format(self.payment_url,
-                               self.payment_id, self.base_url)
+        url = '{}/{}/refunds'.format(self.payment_url,
+                                     self.payment_id)
         responses.add(responses.GET, url, status=200, body=json.dumps(result),
                       match_querystring=True)
         self.assertEqual(self.client.refund.fetch_for_payment(
