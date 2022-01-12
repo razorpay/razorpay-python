f1fa8a6dc3ea664a610970f86c224ea45af01e16
ankitdas13 <ankit.das@razorpay.com>
2022-01-12 13:17:45 +0530

    add case handle

--- razorpay/utility/utility.py
+++ razorpay/utility/utility.py
@@ -22,18 +22,19 @@ class Utility(object):
         return self.verify_signature(msg, razorpay_signature, secret)
 
     def verify_payment_link_signature(self, parameters):
-        payment_id = str(parameters['razorpay_payment_id'])
-        payment_link_id = str(parameters['payment_link_id'])
-        payment_link_reference_id = str(parameters['payment_link_reference_id'])
-        payment_link_status = str(parameters['payment_link_status'])
-        razorpay_signature = str(parameters['razorpay_signature'])
-
+        
+        if 'razorpay_payment_id' in parameters.keys() and 'payment_link_reference_id' in parameters.keys() and 'payment_link_status' in parameters.keys():
+         payment_id = str(parameters['razorpay_payment_id'])
+         payment_link_id = str(parameters['payment_link_id'])
+         payment_link_reference_id = str(parameters['payment_link_reference_id'])
+         payment_link_status = str(parameters['payment_link_status'])
+         razorpay_signature = str(parameters['razorpay_signature'])
+        else:
+          return False
+          
         msg = "{}|{}|{}|{}".format(payment_link_id, payment_link_reference_id, payment_link_status, payment_id)
         
-        if 'secret' in parameters.keys() :
-          secret = str(parameters['secret'])
-        else:
-          secret = str(self.client.auth[1])
+        secret = str(parameters['secret']) if 'secret' in parameters.keys() else str(self.client.auth[1])
 
         return self.verify_signature(msg, razorpay_signature, secret)    
     
@@ -48,10 +49,7 @@ class Utility(object):
 
         msg = "{}|{}".format(payment_id, subscription_id)
 
-        if 'secret' in parameters.keys() :
-          secret = str(parameters['secret'])
-        else:
-          secret = str(self.client.auth[1])
+        secret = str(parameters['secret']) if 'secret' in parameters.keys() else str(self.client.auth[1])
 
         return self.verify_signature(msg, razorpay_signature, secret)
     
