9cbca3109adc727b5fdadcad487284e4f159b73e
ankitdas13 <ankit.das@razorpay.com>
2022-12-12 15:04:42 +0530

    added v3 all versions

--- setup.py
+++ setup.py
@@ -26,6 +26,9 @@ setup(
         # Make sure that this is reflected in .github/workflows/python.yml as well
         "Programming Language :: Python",
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
