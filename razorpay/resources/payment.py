47030986ab7c6248daf19c9dec0f030cdd409318
Shoaib Siddiqui <shoaibmohd113@gmail.com>
2023-07-30 13:41:49 +0530

    + fix bug #79

--- razorpay/resources/payment.py
+++ razorpay/resources/payment.py
@@ -34,7 +34,8 @@ class Payment(Resource):
         """
         return super(Payment, self).fetch(payment_id, data, **kwargs)
 
-    def capture(self, payment_id, amount, data={}, **kwargs): # nosemgrep : python.lang.correctness.common-mistakes.default-mutable-dict.default-mutable-dict
+    # nosemgrep : python.lang.correctness.common-mistakes.default-mutable-dict.default-mutable-dict
+    def capture(self, payment_id, amount, currency, data={}, **kwargs):
         """"
         Capture Payment for given Id
 
@@ -47,6 +48,7 @@ class Payment(Resource):
         """
         url = "{}/{}/capture".format(self.base_url, payment_id)
         data['amount'] = amount
+        data['currency'] = currency if currency else "INR"
         return self.post_url(url, data, **kwargs)
 
     def refund(self, payment_id, amount, data={}, **kwargs):  # pragma: no cover # nosemgrep : python.lang.correctness.common-mistakes.default-mutable-dict.default-mutable-dict
@@ -116,7 +118,7 @@ class Payment(Resource):
         """
         url = "{}/{}/upi_transfer".format(self.base_url, payment_id)
         return self.get_url(url, data, **kwargs)
-    
+
     def refund(self, payment_id, data={}, **kwargs):
         """"
         Create a normal refund
@@ -135,7 +137,7 @@ class Payment(Resource):
             refunds dict
         """
         url = "{}/{}/refunds".format(self.base_url, payment_id)
-        return self.get_url(url, data, **kwargs) 
+        return self.get_url(url, data, **kwargs)
 
     def fetch_refund_id(self, payment_id, refund_id, **kwargs):
         """"
@@ -145,22 +147,22 @@ class Payment(Resource):
             Refund dict
         """
         url = "{}/{}/refunds/{}".format(self.base_url, payment_id, refund_id)
-        return self.get_url(url, {}, **kwargs)  
-  
+        return self.get_url(url, {}, **kwargs)
+
     def edit(self, payment_id, data={}, **kwargs):
         """"
          Update the Payment
         Args:
             data : Dictionary having keys using which order have to be edited
                 'notes' : key value pair as notes
-            
+
             Returns:
             Payment Dict which was edited
         """
         url = '{}/{}'.format(self.base_url, payment_id)
 
-        return self.patch_url(url, data, **kwargs) 
-    
+        return self.patch_url(url, data, **kwargs)
+
     def fetchCardDetails(self, payment_id, **kwargs):
         """"
         Fetch Card Details of a Payment
@@ -184,9 +186,9 @@ class Payment(Resource):
         Returns:
             Payment dict for given Order Id
         """
-        url = "{}/{}".format(self.base_url,'downtimes')
-        return self.get_url(url, {}, **kwargs)        
-    
+        url = "{}/{}".format(self.base_url, 'downtimes')
+        return self.get_url(url, {}, **kwargs)
+
     def fetchDownTimeById(self, downtime_id, **kwargs):
         """"
         Fetch Payment Downtime Details by ID
@@ -199,8 +201,8 @@ class Payment(Resource):
         """
         url = "{}/downtimes/{}".format(self.base_url, downtime_id)
         return self.get_url(url, {}, **kwargs)
-    
-    def createPaymentJson(self ,data={}, **kwargs):
+
+    def createPaymentJson(self, data={}, **kwargs):
         """"
         Create a Payment
 
@@ -213,7 +215,7 @@ class Payment(Resource):
         """
         url = "{}/create/{}".format(self.base_url, 'json')
 
-        return self.post_url(url, data, **kwargs)  
+        return self.post_url(url, data, **kwargs)
 
     def createRecurring(self, data={}, **kwargs):
         """"
@@ -221,7 +223,7 @@ class Payment(Resource):
         Return:
             Recurring Payments dict
         """
-        url = "{}/{}/recurring".format(self.base_url,'create')
+        url = "{}/{}/recurring".format(self.base_url, 'create')
         return self.post_url(url, data, **kwargs)
 
     def createUpi(self, data={}, **kwargs):
@@ -230,7 +232,7 @@ class Payment(Resource):
         Return:
           Payments dict
         """
-        url = "{}/create/{}".format(self.base_url,'upi')
+        url = "{}/create/{}".format(self.base_url, 'upi')
         return self.post_url(url, data, **kwargs)
 
     def validateVpa(self, data={}, **kwargs):
@@ -239,8 +241,8 @@ class Payment(Resource):
         Return:
           Payments dict
         """
-        url = "{}/validate/{}".format(self.base_url,'vpa')
-        return self.post_url(url, data, **kwargs)            
+        url = "{}/validate/{}".format(self.base_url, 'vpa')
+        return self.post_url(url, data, **kwargs)
 
     def fetchPaymentMethods(self, **kwargs):
         """"
@@ -250,7 +252,7 @@ class Payment(Resource):
         """
         url = "/{}".format('methods')
         return self.get_url(url, {}, **kwargs)
-    
+
     def otpGenerate(self, payment_id, data={}, **kwargs):
         """"
         Otp Generate
