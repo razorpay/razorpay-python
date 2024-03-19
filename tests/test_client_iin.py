3320a23973df23e8dfcda3a32fa25841607bcf74
Ankit Das <89454448+ankitdas13@users.noreply.github.com>
2024-03-19 15:52:44 +0530

    Updated iin (#274)

--- tests/test_client_iin.py
+++ tests/test_client_iin.py
@@ -18,3 +18,12 @@ class TestClientIin(ClientTestCase):
         responses.add(responses.GET, url, status=200, body=json.dumps(result),
                       match_querystring=True)
         self.assertEqual(self.client.iin.fetch(self.token_iin), result)
+
+    @responses.activate
+    def test_addon_fetch(self):
+        result = mock_file('iin_collection')
+        query = 'sub_type=otp'
+        url = f"{self.base_url}/list?{query}"
+        responses.add(responses.GET, url, status=200, body=json.dumps(result),
+                      match_querystring=True)
+        self.assertEqual(self.client.iin.all({"sub_type":"otp"}), result)
