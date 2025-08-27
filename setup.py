f0f8061ee4e80d908b4bd10a4103d3e45399369a
manish soni <manish.soni@razorpay.com>
2025-08-27 12:28:58 +0530

    chore: bump version to 1.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme:
 
 setup(
     name="razorpay",
-    version="1.4.2",
+    version="1.5.0",
     description="Razorpay Python Client",
     long_description=readme_content,
     long_description_content_type='text/markdown',
